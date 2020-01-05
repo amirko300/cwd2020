b85b5f7980b0f7a9f5b162d2ea31e409aef4c3a9
Amir Kornet <amirk@ibo1.com>
2020-01-05 17:00:12 +0200

    java script function12

--- learnJs/exe5.js
+++ learnJs/exe5.js
@@ -7,3 +7,20 @@ function checkDriverAge(age) {
         console.log("Congratulations on your first year of driving. Enjoy the ride!");
     } 
 }
+
+checkDriverAge2 = (age) => {
+    if (Number(age) < 18) {
+        console.log("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        console.log("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        console.log("Congratulations on your first year of driving. Enjoy the ride!");
+    }   
+}
+
+
+
+checkDriverAge(12);
+checkDriverAge(18);
+checkDriverAge(33);
+checkDriverAge2(8);
