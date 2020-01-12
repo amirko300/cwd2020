62de05d9c5dbc3698e4c722de736b0e2f3b4bbad
amirko300 <amirko300@gmail.com>
2020-01-12 19:13:01 +0200

    js objects

---
+++ learnJs/js1/exe7.js
@@ -0,0 +1,22 @@
+var user = {
+    username: "Amir",
+    password: "1234"
+};
+
+var feed1 = {
+    username: "amir",
+    timeline: "i using facebook"
+},
+
+    feed2 = {
+        username: "john",
+        timeline: "i using facebook too"
+    },
+
+    feed3 = {
+        username: "bill",
+        timeline: "i just try it"
+    };
+
+var database = [user];
+var newsfeed = [feed1, feed2, feed3];
