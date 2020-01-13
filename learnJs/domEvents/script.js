faeafcfcbb51334b162f0b568fdd854480ee5d7b
amirko300 <amirko300@gmail.com>
2020-01-13 07:54:09 +0200

    js events exe1

--- learnJs/domEvents/script.js
+++ learnJs/domEvents/script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
 
 function inputLength() {
 	return input.value.length;
@@ -25,6 +26,15 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function addDecoration(event) {
+	event.target.classList.toggle("done");
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+li.forEach(function (item) {
+	item.addEventListener("click", addDecoration);
+});
+
